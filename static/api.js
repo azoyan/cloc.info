9e3c84840ab38026a884f60f7c14a80915b9332c
Ivan Azoyan <azoyan.89@mail.ru>
2023-10-03 11:56:08 +0300

    improve view of statistic

--- static/api.js
+++ static/api.js
@@ -5,12 +5,7 @@ class Api {
     recent(response) {
         let element = document.getElementById(`recent`)
 
-        response.sort((a, b) => {
-            console.log("a", a)
-            if (a.time > b.time) return -1;
-            if (a.time < b.time) return 1;
-            return 0
-        })
+        response.sort(sort_recent)
         for (let i = 0; i < response.length; ++i) {
             let item = new ListItem(response[i]).recent()
             element.appendChild(item)
@@ -19,12 +14,7 @@ class Api {
 
     popular(response) {
         let element = document.getElementById(`popular`)
-
-        response.sort((a, b) => {
-            if (a.count > b.count) return -1;
-            if (a.count < b.count) return 1;
-            return 0
-        })
+        response.sort(sort_popular)
         for (let i = 0; i < response.length; ++i) {
             let item = new ListItem(response[i]).popular()
             element.appendChild(item)
@@ -34,16 +24,28 @@ class Api {
     largest(response) {
         let element = document.getElementById(`largest`)
 
-        response.sort((a, b) => {
-            if (a.size > b.size) return -1;
-            if (a.size < b.size) return 1;
-            return 0
-        })
+        response.sort(sort_largest)
         for (let i = 0; i < response.length; ++i) {
             let item = new ListItem(response[i]).largest()
             element.appendChild(item)
         }
     }
+}
+
+function sort_recent(a, b) {
+    if (a.time > b.time) return -1;
+    if (a.time < b.time) return 1;
+    return 0
+}
+function sort_popular(a, b) {
+    if (a.count > b.count) return -1;
+    if (a.count < b.count) return 1;
+    return 0
+}
+function sort_largest(a, b) {
+    if (a.size > b.size) return -1;
+    if (a.size < b.size) return 1;
+    return 0
 }
 
 function start() {
@@ -74,7 +76,8 @@ function createExternalLink(icon) {
 
     buttonGroup.setAttribute("role", "group")
     buttonGroup.setAttribute("aria-label", "Link to repository")
-
+    // buttonGroup.style.width = "32px"
+    // buttonGroup.style.height = "32px"
     let iconButton = document.createElement("button")
     iconButton.setAttribute("type", "button")
     iconButton.classList.add("btn", "btn-outline", "border", "pe-none")
@@ -91,7 +94,7 @@ function createExternalLink(icon) {
     externalIcon.classList.add("bi", "bi-box-arrow-up-right", "align-top");
     externalButton.appendChild(externalIcon)
 
-    buttonGroup.appendChild(externalButton)
+    // buttonGroup.appendChild(externalButton)
 
     return buttonGroup
 }
@@ -99,41 +102,38 @@ function createExternalLink(icon) {
 function createExternalLinks(repository) {
     let icon;
     if (repository.hostname === "github.com") {
-        let svg = `<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-github" viewBox="-2 -2 18 18"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/></svg>`
+        let svg = `<svg xmlns="http://www.w3.org/2000/svg" width="18.75" height="18.75" fill="currentColor" class="bi bi-github" viewBox="-1 -3 18 18"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/></svg>`
         icon = new DOMParser().parseFromString(svg, "text/xml").firstChild
     }
     else if (repository.hostname === "gitlab.com") {
         icon = document.createElement("img")
-        icon.setAttribute("alt", "Open repository")
         icon.setAttribute("width", "18px")
-        icon.setAttribute("height", "18px")
+        icon.setAttribute("height", "22px")
         icon.setAttribute("src", "/static/gitlab-3.svg")
-        icon.classList.add("float-start", "pe-1")
+        icon.classList.add("float-start")
     }
     else if (repository.hostname === "bitbucket.org") {
         icon = document.createElement("img")
-        icon.setAttribute("alt", "Open repository")
-        icon.setAttribute("width", "18px")
-        icon.setAttribute("height", "18px")
+        icon.setAttribute("width", "20px")
+        icon.setAttribute("height", "22px")
         icon.setAttribute("src", "/static/bitbucket-icon.svg")
-        icon.classList.add("float-start", "pe-1")
+        icon.classList.add("float-start")
     }
     else if (repository.hostname === "codeberg.org") {
-        let svg = `<svg style="color: #2185d0" role="img" width="16px" height="15px" viewBox="-3 0 27 27" xmlns="http://www.w3.org/2000/svg"><title>Codeberg</title>
+        let svg = `<svg style="color: #2185d0" role="img" width="18px" height="18px" viewBox="-4 0 28 28" xmlns="http://www.w3.org/2000/svg"><title>Codeberg</title>
                 <path d="M11.955.49A12 12 0 0 0 0 12.49a12 12 0 0 0 1.832 6.373L11.838 5.928a.187.14 0 0 1 .324 0l10.006 12.935A12 12 0 0 0 24 12.49a12 12 0 0 0-12-12 12 12 0 0 0-.045 0zm.375 6.467l4.416 16.553a12 12 0 0 0 5.137-4.213z" fill="#2185d0"></path>
             </svg>`
         icon = new DOMParser().parseFromString(svg, "text/xml").firstChild
     }
     else if (repository.hostname === "gitea.com") {
         icon = document.createElement("img")
-        icon.setAttribute("alt", "Open repository")
-        icon.setAttribute("width", "22px")
+        icon.setAttribute("width", "18px")
         icon.setAttribute("height", "22px")
         icon.setAttribute("src", "/static/gitea.svg")
-        icon.classList.add("float-start", "pe-1")
+        icon.classList.add("float-start")
     }
     else {
-        let svg = `<svg xmlns="http://www.w3.org/2000/svg" width="16" style="color: #f34f29;" height="16" fill="currentColor" class="bi bi-git" viewBox="0 0 16 16">
+        let svg = `<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" style="color: #f34f29;" fill="currentColor" class="bi bi-git" viewBox="0 0 16 16">
         <path d="M15.698 7.287 8.712.302a1.03 1.03 0 0 0-1.457 0l-1.45 1.45 1.84 1.84a1.223 1.223 0 0 1 1.55 1.56l1.773 1.774a1.224 1.224 0 0 1 1.267 2.025 1.226 1.226 0 0 1-2.002-1.334L8.58 5.963v4.353a1.226 1.226 0 1 1-1.008-.036V5.887a1.226 1.226 0 0 1-.666-1.608L5.093 2.465l-4.79 4.79a1.03 1.03 0 0 0 0 1.457l6.986 6.986a1.03 1.03 0 0 0 1.457 0l6.953-6.953a1.031 1.031 0 0 0 0-1.457"/>
         </svg>`
         icon = new DOMParser().parseFromString(svg, "text/xml").firstChild
@@ -209,24 +209,30 @@ class ListItem {
         let local_href = "/" + repository.hostname + "/" + repository.owner + "/" + repository.repository_name
 
         let row = createRow();
-        let col1 = createColumn("text-truncate")
+        let col1 = createColumn("col-sm", "text-truncate")
 
         let a = document.createElement("a")
         a.setAttribute("target", "_blank")
         a.setAttribute("rel", "noopener noreferrer canonical")
         a.setAttribute("href", local_href)
+        let title = `${repository.repository_name}`
+        a.setAttribute("title", title)
+
         a.classList.add("link-dark", "me-2")
         a.innerText = repository.repository_name
         col1.appendChild(a)
         row.appendChild(col1)
 
-        col1.appendChild(this.description)
+        let col2 = createColumn("col-auto")
+        col2.appendChild(this.description)
+        row.appendChild(col2)
 
         let col3 = document.createElement("a")
         col3.setAttribute("target", "_blank")
         col3.setAttribute("rel", "noopener noreferrer canonical")
         let href = 'https://' + repository.hostname + "/" + repository.owner + "/" + repository.repository_name
         col3.setAttribute("href", href)
+        col3.setAttribute("title", `Open repository ${href}`)
         col3.classList.add("col", "col-auto")
         let externalLink = createExternalLinks(repository);
         col3.appendChild(externalLink)
@@ -243,7 +249,8 @@ class ListItem {
 
             listItem.appendChild(row)
             listItem.appendChild(collapse)
-        } else {
+        }
+        else {
             listItem.appendChild(row)
         }
 
@@ -312,8 +319,8 @@ function createLargestItem(repository) {
 function createColumn(...tokens) {
     let col = document.createElement("div")
     col.classList.add("col")
-    if (tokens.length > 0) {
-        col.classList.add(tokens)
+    for (let i = 0; i < tokens.length; i++) {
+        col.classList.add(tokens[i])
     }
     return col
 }
@@ -334,6 +341,7 @@ class CollapseContent {
         this.elements = [];
     }
     recent() {
+        this.repository_array.sort(sort_recent)
         for (let i = 0; i < this.repository_array.length; ++i) {
             let time = this.repository_array[i].time;
             let now = Date.now()
@@ -345,6 +353,7 @@ class CollapseContent {
         return this.toElement()
     }
     largest() {
+        this.repository_array.sort(sort_largest)
         for (let i = 0; i < this.repository_array.length; ++i) {
             let size = this.repository_array[i].size;
             let bytes = formatBytes(size)
@@ -355,6 +364,7 @@ class CollapseContent {
     }
 
     popular() {
+        this.repository_array.sort(sort_popular)
         for (let i = 0; i < this.repository_array.length; ++i) {
             let count = this.repository_array[i].count;
             let text = `${count} view`
@@ -367,58 +377,30 @@ class CollapseContent {
     toElement() {
         let div = document.createElement("div");
         div.id = this.id;
-        div.classList.add("collapse", "row");
+        div.classList.add("collapse", "row", "mt-2");
 
         for (let i = 0; i < this.repository_array.length; ++i) {
             let row = createRow();
 
-            let col1 = createColumn("col-auto");
-            row.appendChild(col1);
+            let col1 = createColumn("col-auto", "text-truncate");
 
             let a = document.createElement("text");
             a.innerText = this.repository_array[i].branch_name;
 
             col1.appendChild(a);
-            let col2 = createColumn("col-auto");
+            row.appendChild(col1);
+
+            let col2 = createColumn("col-auto", "text-truncate");
             col2.appendChild(this.elements[i]);
             row.appendChild(col2);
 
+            // let col3 = createColumn("col-sm");
+            // row.appendChild(col3)
             div.appendChild(row);
         }
 
         return div;
     }
-}
-
-function createCollapseContent(id, repository_array, s, key) {
-    let div = document.createElement("div")
-    div.id = id
-    div.classList.add("collapse", "row")
-
-    for (let i = 0; i < repository_array.length; ++i) {
-        let row = createRow()
-        let r = repository_array[i]
-
-        let col1 = createColumn("col-auto")
-        row.appendChild(col1)
-
-        let a = document.createElement("text")
-
-        a.innerText = repository_array[i].branch_name
-
-
-        col1.appendChild(a)
-        let smallText = createSmallText(s.replace("{ }", r[key]))
-
-        let col2 = createColumn("col-auto")
-        col2.appendChild(smallText)
-        row.appendChild(col2)
-        // row.appendChild(createExternalLinks(repository_array[i]))
-
-        div.appendChild(row)
-    }
-
-    return div
 }
 
 function createSmallText(text) {
