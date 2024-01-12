20a8c13e0769f196380e8555428a3ae39d9e53d1
Ivan Azoyan <azoyan.89@mail.ru>
2024-01-13 01:58:14 +0300

    replace bootstrap icon from cdn to svg

--- static/api.js
+++ static/api.js
@@ -428,10 +428,17 @@ function createCollapseButton(id) {
     button.setAttribute("aria-controls", id)
     button.setAttribute("aria-label", "Show more")
 
-    let icon = document.createElement("i")
-    icon.classList.add("text-secondary", "bi", "bi-chevron-expand")
+    // let icon = document.createElement("i")
+    // icon.classList.add("text-secondary", "bi", "bi-chevron-expand")
 
-    button.appendChild(icon)
+    let html = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-expand" viewBox="0 0 16 16">
+    <path fill-rule="evenodd" d="M3.646 9.146a.5.5 0 0 1 .708 0L8 12.793l3.646-3.647a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 0-.708m0-2.292a.5.5 0 0 0 .708 0L8 3.207l3.646 3.647a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 0 0 0 .708"/>
+  </svg>`
+
+    let tmp = document.createElement("div");
+    let element = new DOMParser().parseFromString(html, "text/html");
+    tmp.innerHTML = html;
+    button.appendChild(tmp.firstChild)
     return button
 }
 
